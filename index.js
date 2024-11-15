bbe203a4606694d13e2f6c8d966334b97c991db0
Usitha <103616801+Usitha123@users.noreply.github.com>
2024-11-15 09:05:43 +0530

    update localhost

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
 });
 
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
 module.exports = app;
